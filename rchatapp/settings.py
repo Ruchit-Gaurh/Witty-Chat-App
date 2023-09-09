9c1ab2fe8629aeb0c89ba5363d0206233a145766
Ruchit <ruchitgaurh6989@gmail.com>
2023-09-09 18:23:06 +0530

    commit

--- rchatapp/settings.py
+++ rchatapp/settings.py
@@ -126,3 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS=['https://witty.up.railway.app']
