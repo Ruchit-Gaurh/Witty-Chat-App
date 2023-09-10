a7cb5383882389d16f6df10bc9b45dd70f749002
Ruchit <ruchitgaurh6989@gmail.com>
2023-09-10 10:48:58 +0530

    SECRET_KEY as env variable

--- rchatapp/settings.py
+++ rchatapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z4o16x705m$ar*kv$9@gsj0k8#s72k!yx=&hn8x1q$4kf8t%e+"
+# SECRET_KEY = "django-insecure-z4o16x705m$ar*kv$9@gsj0k8#s72k!yx=&hn8x1q$4kf8t%e+"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'z4o16x705m$ar*kv$9@gsj0k8#s72k!yx=&hn8x1q$4kf8t%e+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ["witty.up.railway.app", "127.0.0.1"]
 
